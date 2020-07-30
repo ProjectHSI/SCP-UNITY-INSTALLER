55e792e104eab836b7d82a445845e000149665cf
ProjectHSI <57279339+ProjectHSI@users.noreply.github.com>
2020-07-30 20:03:24 +1000

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ var UpdateScript = async () => {
 	if (fs.readFileSync('./version.json') == json) {
     console.log("Your version of this software is update to date!");
   } else {
-      const response = await fetch('https://raw.githubusercontent.com/ProjectHSI/SCP-UNITY-INSTALLER/master/index.js');
-    	const text = await response.text();
-      fs.writeFileSync("./index.js", text)
-      fs.writeFileSync("./version.json", json)
-      console.log("Please restart the program. I will exit for you.");
-      process.exit(0)
+    var response = fetch('https://raw.githubusercontent.com/ProjectHSI/SCP-UNITY-INSTALLER/master/index.js');
+    const text = response.text();
+    fs.writeFileSync("./index.js", text)
+    fs.writeFileSync("./version.json", json)
+    console.log("Please restart the program. I will exit for you.");
+    process.exit(0)
   }
 }
-UpdateScript()
 //This is to know if my code can update
