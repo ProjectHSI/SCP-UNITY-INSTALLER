ddf8b910f1d51348dc11a34e4b590724c79c7c22
ProjectHSI <57279339+ProjectHSI@users.noreply.github.com>
2020-07-30 19:59:52 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const fetch = require('node-fetch');
 const fs = require('fs')
-(async () => {
+var UpdateScript = async () => {
 	const response = await fetch('https://raw.githubusercontent.com/ProjectHSI/SCP-UNITY-INSTALLER/master/version.json');
 	const json = await response.json();
 	if (fs.readFileSync('./version.json') == json) {
     console.log("Your version of this software is update to date!");
   } else {
-    (await () => {
       const response = await fetch('https://raw.githubusercontent.com/ProjectHSI/SCP-UNITY-INSTALLER/master/index.js');
     	const text = await response.text();
       fs.writeFileSync("./index.js", text)
       fs.writeFileSync("./version.json", json)
       console.log("Please restart the program. I will exit for you.");
       process.exit(0)
-    })()
   }
-})();
+})
+UpdateScript()
 //This is to know if my code can update
