f572e9c32c36d2b3f899ea3cd40d6b761c687eb6
ProjectHSI <57279339+ProjectHSI@users.noreply.github.com>
2020-07-30 19:39:08 +1000

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const fs = require('fs')
 	if (fs.readFileSync('./version.json') == json) {
     console.log("Your version of this software is update to date!");
   } else {
-
+    (await () => {
+      const response = await fetch('https://raw.githubusercontent.com/ProjectHSI/SCP-UNITY-INSTALLER/master/index.js');
+    	const text = await response.text();
+      fs.writeFileSync("./index.js", text)
+      console.log("Please restart the program. I will exit for you.");
+      process.exit(0)
+    })()
   }
 })();
